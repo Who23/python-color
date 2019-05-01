ec1d78e31887ef28446431338e9d08ff3a82f725
Who23 <40632266+Who23@users.noreply.github.com>
2019-05-01 19:05:16 -0400

    sync

--- cmdline.py
+++ cmdline.py
@@ -2,6 +2,7 @@ import sys, tty, termios, code
 
 old_settings = termios.tcgetattr(sys.stdin)
 tty.setraw(sys.stdin)
+#column, row
 cursorIndex = [0, 0]
 line = ""
 i = code.InteractiveInterpreter()
@@ -14,28 +15,31 @@ with open("keywords.txt") as keywordstxt:
     
 
 def formatLine(line):
+    #actually format it with color!
     line = line.split(" ")
     for index, word in enumerate(line):
-        ending = ""
         for func in builtins:
             if func in word:
                 temp = word.split(func)
                 temp0 = len(temp[0])
                 temp1 = len(temp[1])
                 
+                #make sure functions are being color coded properly, even if surrounded by 
+                #any parentheses
                 if ((not temp0) or temp[0][-1] in "([{}])") and ((not temp1) or temp[1][0] in "([{}])"):
                      line[index] = temp[0] + "\u001b[35m" + func + "\u001b[0m" + temp[1]
                     
-
-        
-
         # if word in builtins:
-        #     
         if word in keywords:
-            if not ending:
-                line[index] = "\u001b[33m" + word + "\u001b[0m"
+            line[index] = "\u001b[33m" + word + "\u001b[0m"
+
+    line = ' '.join(line).split("\n")
+    line[0] = ">>> " + line[0]
+    for index, word in enumerate(line[1:]):
+        line[index+1] = "... " + word
+    line = '\n'.join(line)
         
-    return ' '.join(line)
+    return line
 
 
 while True:
@@ -63,7 +67,12 @@ while True:
         #backspace
         elif typed == 127:
             if cursorIndex[0] != 0:
-                line = line[:cursorIndex[0]-1] + line[cursorIndex[0]:]
+                tempLine = line.split("\n")
+                tempLine1 = tempLine[cursorIndex[1]]
+                tempLine1 = tempLine1[:cursorIndex[0]-1] + tempLine1[cursorIndex[0]:]
+                tempLine[cursorIndex[1]] = tempLine1
+                line = "\n".join(tempLine)
+                
                 cursorIndex[0] -= 1
 
         #tab
@@ -78,6 +87,7 @@ while True:
                 result = code.compile_command(line)
             except Exception as e:
                 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+                print()
                 print(e)
                 tty.setraw(sys.stdin)
                 result = False
@@ -85,6 +95,7 @@ while True:
             finally:
                 if result == None and result != False:
                     line += "\n"
+                    cursorIndex[1] += 1
                     print()
                 else:
                     sys.stdout.write("\n\u001b[1000D")
@@ -93,6 +104,7 @@ while True:
                     tty.setraw(sys.stdin)
                     line = ""
                     sys.stdout.write("\n\u001b[1B")
+                    cursorIndex[1] = 0
                 cursorIndex[0] = 0
 
         else:
@@ -105,10 +117,15 @@ while True:
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
         newlinecount = line.count("\n")
         if newlinecount:
-             sys.stdout.write(f"\u001b[K\u001b[{newlinecount}A")
-        print("\u001b[1000D\u001b[K" + formatted_line, end="\r")
+            #moves back to first line and cleans each line on the way
+            for x in range(newlinecount):
+                sys.stdout.write(f"\r\u001b[K\u001b[1A")
+
+        #using print instead of sys.stdout.write b/c if @ the botton of the screen
+        #it doesn't add a new line and messes everything up
+        print("\u001b[1000D\u001b[K" + formatted_line, end="")
         if cursorIndex[0]:
-            sys.stdout.write(f"\u001b[1000D\u001b[{cursorIndex[0]}C")
+            sys.stdout.write(f"\u001b[1000D\u001b[{cursorIndex[0]+4}C")
         sys.stdout.flush()
         tty.setraw(sys.stdin)
 
