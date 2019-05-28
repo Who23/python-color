ccf269c6fa2ec8148620a6786ee95e733f8e1e83
Who23 <40632266+Who23@users.noreply.github.com>
2019-05-28 14:50:50 -0400

    changed setup script and updated README

--- setup.sh
+++ setup.sh
@@ -5,14 +5,21 @@ if [[ $2 -eq 1 ]]
 then
     echo "not removing setup.sh :)"
 else
-    rm setup.sh
+    rm ./setup.sh
 fi
-#mv ../python-color ~/.python-color
+
+mv ../python-color ~/.python-color
 
 pathtest="$(echo $PATH | grep 'python-color')"
 if [[ $pathtest == $PATH ]]
 then
     echo "python-color has already been installed in PATH"
 else
-    echo 'export PATH="$($HOME/.python-color)"' >> ~/$1
+    echo 'PATH=$PATH:$HOME/.python-color' | cat - ~/$1 > temp && mv temp ~/$1
+    echo "modified PATH in ~/$1 at line 1"
 fi
+
+echo -e '\033[34m=====================================================\033[0m'
+echo -e '\033[32m\033[1mpython-color\033[0m\033[1m has been installed!'
+echo -e 'restart your terminal and type \033[32mpython-color\033[0m\033[1m to use!\033[0m'
+echo -e '\033[34m=====================================================\033[0m'
