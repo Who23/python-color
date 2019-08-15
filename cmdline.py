36889dfb9f2f3562823edf03817cb96d42689921
Who23 <40632266+Who23@users.noreply.github.com>
2019-08-14 23:52:52 -0400

    add space before comments

--- cmdline.py
+++ cmdline.py
@@ -2,7 +2,7 @@ import sys, tty, termios, code, os, re
 
 old_settings = termios.tcgetattr(sys.stdin)
 tty.setraw(sys.stdin)
-#column, row
+# column, row
 cursorIndex = [0, 0]
 prevLines = []
 hasTyped = False
@@ -11,11 +11,11 @@ upArrowOnWhich = 0
 line = ""
 i = code.InteractiveInterpreter()
 
-#get our builtins to highlight
+# get our builtins to highlight
 with open(f'{os.path.dirname(os.path.abspath(__file__))}/builtins.txt') as builtinstxt:
     builtins = [line.rstrip() for line in builtinstxt]
 
-#get our keywords to highlight
+# get our keywords to highlight
 with open(f'{os.path.dirname(os.path.abspath(__file__))}/keywords.txt') as keywordstxt:
     keywords = [line.rstrip() for line in keywordstxt]
     
@@ -59,79 +59,79 @@ while True:
     try:
         typed = ord(sys.stdin.read(1))
 
-        #check for special key presses
-        #CTRL+C
+        # check for special key presses
+        # CTRL+C
         if typed == 3:
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
             print()
             quit()
         
-        #arrow keys
+        # arrow keys
         elif typed == 27:
             _, typed = sys.stdin.read(2)
 
-            #up arrow
+            # up arrow
             if ord(typed) == 65:
-                #if the user has not typed anything on the line they are on
+                # if the user has not typed anything on the line they are on
                 if not hasTyped:
-                    #increase the command history ref index by 1
+                    # increase the command history ref index by 1
                     if upArrowOnWhich < len(prevLines):
                         upArrowOnWhich += 1
 
-                        #modify that line to the line in the command history
+                        # modify that line to the line in the command history
                         a = line.split("\n")
                         a[cursorIndex[1]] = prevLines[-1*upArrowOnWhich]
                         cursorIndex[0] = len(prevLines[-1*upArrowOnWhich])
                         line = '\n'.join(a)
 
                 else:
-                    #move up the cursor unless we're at the top
-                    #print(cursorIndex[1])
+                    # move up the cursor unless we're at the top
+                    # print(cursorIndex[1])
                     if cursorIndex[1] > 0:
                         cursorIndex[1] -= 1
                         sys.stdout.write("\u001b[1A")
 
-            #down arrow
+            # down arrow
             elif ord(typed) == 66:
-                #if we aren't in a new line and the user has not typed anything on the line they're on
-                #print(hasTyped)
+                # if we aren't in a new line and the user has not typed anything on the line they're on
+                # print(hasTyped)
                 if line.split("\n")[cursorIndex[1]] != "" and not hasTyped:
-                    #decrease the command history ref index by 1
+                    # decrease the command history ref index by 1
                     upArrowOnWhich -= 1 if upArrowOnWhich > 0 else 0
 
-                    #if we aren't back at the fresh line
+                    # if we aren't back at the fresh line
                     if upArrowOnWhich != 0:
-                        #modify that line
+                        # modify that line
                         a = line.split("\n")
                         a[cursorIndex[1]] = prevLines[-1*upArrowOnWhich]
                         line = '\n'.join(a)
 
-                        #set the cursor index correctly
+                        # set the cursor index correctly
                         cursorIndex[0] = len(prevLines[-1*upArrowOnWhich])
                     else:
-                        #if it is the fresh line, clear it out
+                        # if it is the fresh line, clear it out
                         line = ""
                         cursorIndex[0] = 0
                 else:
-                    #moves down the cursor if we aren't at the bottom
+                    # moves down the cursor if we aren't at the bottom
                     if cursorIndex[1] != line.count("\n"):
                         cursorIndex[1] += 1
                         sys.stdout.write("\u001b[1B")
 
-            #right arrow
+            # right arrow
             elif ord(typed) == 67:
                 cursorIndex[0] += 1 if cursorIndex[0] != len(line.split("\n")[cursorIndex[1]]) else 0
                 hasTyped = True if cursorIndex[0] != len(line.split("\n")[cursorIndex[1]]) else False
 
-            #left arrow
+            # left arrow
             elif ord(typed) == 68:
                 cursorIndex[0] -= 1 if cursorIndex[0] != 0 else 0
                 hasTyped = True
 
-        #backspace
+        # backspace
         elif typed == 127:
             if cursorIndex[0] != 0:
-                #delete the character at the position of the cursor
+                # delete the character at the position of the cursor
                 tempLine = line.split("\n")
                 tempLine1 = tempLine[cursorIndex[1]]
                 tempLine1 = tempLine1[:cursorIndex[0]-1] + tempLine1[cursorIndex[0]:]
@@ -141,20 +141,20 @@ while True:
                 cursorIndex[0] -= 1
                 hasTyped = True
 
-        #tab
+        # tab
         elif typed == 9:
             line += "    "
             cursorIndex[0] += 4
             hasTyped = True
             
-        #enter
+        # enter
         elif typed == 13:
             try:
-                #compile the command
+                # compile the command
                 result = code.compile_command(line)
             except Exception as e:
-                #if there's an error in the command, it will throw an error
-                #print it out and reset
+                # if there's an error in the command, it will throw an error
+                # print it out and reset
                 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
                 print()
                 print(e)
@@ -163,34 +163,34 @@ while True:
                 line = ""
             finally:
                 if result == None and result != False:
-                    #None means that the command isn't finished, and more input is needed
+                    # None means that the command isn't finished, and more input is needed
                     line += "\n"
                     cursorIndex[1] += 1
                     upArrowOnWhich = 0
                     print()
                 else:
-                    #if we're good, run the code
+                    # if we're good, run the code
                     sys.stdout.write("\n\u001b[1000D")
                     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
                     i.runcode(result)
                     tty.setraw(sys.stdin)
 
-                    #add line to command line history
+                    # add line to command line history
                     for miniLine in line.split("\n"):
                         if miniLine != "":
                             prevLines.append(miniLine)
 
-                    #reset stuff
+                    # reset stuff
                     line = ""
                     hasTyped = False
                     upArrowOnWhich = 0
                     cursorIndex[1] = 0
                 cursorIndex[0] = 0
 
-        #anything else
+        # anything else
         else:
             tempLine = line.split("\n")
-            #insert the typed character
+            # insert the typed character
             tempLine1 = tempLine[cursorIndex[1]]
             tempLine1 = tempLine1[:cursorIndex[0]] + chr(typed) + tempLine1[cursorIndex[0]:]
             tempLine[cursorIndex[1]] = tempLine1
@@ -200,34 +200,34 @@ while True:
             cursorIndex[0] += 1
 
         if line == "":
-            #if we're on a new line, let the user use the command line history
+            # if we're on a new line, let the user use the command line history
             hasTyped = False
 
 
 
         formatted_line = formatLine(line)
 
-        ## write out to the terminal, moving cursor as well ##
+        # # write out to the terminal, moving cursor as well ##
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
         newlinecount = line.count("\n")
 
-        #move the cursor to the bottom of the text
+        # move the cursor to the bottom of the text
         for x in range(newlinecount - cursorIndex[1]):
             sys.stdout.write("\u001b[1B")
         
 
-        #if we have newlines, write them out
+        # if we have newlines, write them out
         if newlinecount:
-            #moves back to first line and cleans each line on the way
+            # moves back to first line and cleans each line on the way
             for x in range(newlinecount):
                 sys.stdout.write("\r\u001b[K\u001b[1A")
 
 
-        #using print instead of sys.stdout.write b/c if @ the botton of the screen
-        #it doesn't add a new line and messes everything up
+        # using print instead of sys.stdout.write b/c if @ the botton of the screen
+        # it doesn't add a new line and messes everything up
         print("\u001b[1000D\u001b[K" + formatted_line + "\u001b[1000D\u001b[4C", end="")
             
-        #move the cursors into their appropriate spots
+        # move the cursors into their appropriate spots
         if cursorIndex[1] != newlinecount:
             upByHowMuch = newlinecount - cursorIndex[1]
             sys.stdout.write(f"\u001b[{upByHowMuch}A")
@@ -238,7 +238,7 @@ while True:
         sys.stdout.flush()
         tty.setraw(sys.stdin)
 
-    #except any errors and quit nicely
+    # except any errors and quit nicely
     except Exception as e:
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
         print("\n\u001b[31mError in python-color!\u001b[0m")
